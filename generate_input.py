c39c030f42a7ac1baf28b99e663faf9f56edffdf
Pochang Lee <pochanglee2008@gmail.com>
2022-03-07 08:29:44 +0800

    update

---
+++ generate_input.py
@@ -0,0 +1,10 @@
+length = 100000
+print(length)
+
+for index in range(length):
+    print(index, end=' ')
+print()
+
+for _ in range(length):
+    print(1000, end=' ')
+print()
--- main.cpp
+++ main.cpp
@@ -2,19 +2,26 @@
 
 using namespace std;
 
-//Define the structs Workshops and Available_Workshops.
-//Implement the functions initialize and CalculateMaxWorkshops
+// Define the structs Workshops and Available_Workshops.
+// Implement the functions initialize and CalculateMaxWorkshops
 
-struct Workshops
+struct Workshop
 {
     int start_time;
     int end_time;
     int duration;
+
+    Workshop(int _start_time, int _duration)
+    {
+        start_time = _start_time;
+        duration = _duration;
+        end_time = start_time + duration;
+    }
 };
 
 struct Available_Workshops
 {
-    vector<Workshops> workshops;
+    vector<Workshop> workshops;
 };
 
 Available_Workshops *initialize(int start_time[], int duration[], int n)
@@ -22,11 +29,7 @@ Available_Workshops *initialize(int start_time[], int duration[], int n)
     Available_Workshops *aw = new Available_Workshops();
     for (int i = 0; i < n; i++)
     {
-        aw->workshops.push_back(Workshops());
-        Workshops &w = aw->workshops[i];
-        w.start_time = start_time[i];
-        w.duration = duration[i];
-        w.end_time = w.start_time + w.duration;
+        aw->workshops.push_back(Workshop(start_time[i], duration[i]));
     }
     return aw;
 };
@@ -39,30 +42,78 @@ int max(int a, int b)
         return b;
 }
 
-bool earlier(Workshops &a, Workshops &b)
+template <typename DataType, typename ReturnType>
+ReturnType reduce(function<ReturnType(const ReturnType &, const DataType &)> func, const vector<DataType> &v, const ReturnType &initial)
 {
-    return a.start_time < b.start_time;
-}
-
-int search(vector<Workshops> &workshops, int start_index, int start_time)
-{
-    int result = 0;
-    for (; start_index < workshops.size(); start_index++)
+    ReturnType result = initial;
+    for (const DataType &d : v)
     {
-        Workshops &ws = workshops[start_index];
-        if (ws.start_time >= start_time)
-        {
-            int count = search(workshops, start_index + 1, ws.end_time);
-            result = max(count + 1, result);
-        }
+        result = func(result, d);
     }
     return result;
 }
 
+struct Candidate
+{
+    int start_time;
+    int count;
+
+    Candidate(int _start_time, int _count)
+    {
+        start_time = _start_time;
+        count = _count;
+    }
+};
+
+bool later(Workshop &a, Workshop &b)
+{
+    return a.start_time > b.start_time;
+}
+
+function<bool(const Candidate &)> after(int end_time)
+{
+    return [end_time](const Candidate &c) -> bool
+    {
+        return c.start_time >= end_time;
+    };
+}
+
+template <typename DataType>
+function<bool(const DataType &)> inv(function<bool(const DataType &)> func)
+{
+    return [func](const DataType &d) -> bool
+    {
+        return !func(d);
+    };
+}
+
+int reducer(const int &count, const Candidate &c)
+{
+    return max(c.count, count);
+}
+
 int CalculateMaxWorkshops(Available_Workshops *ptr)
 {
-    sort(ptr->workshops.begin(), ptr->workshops.end(), earlier);
-    return search(ptr->workshops, 0, 0);
+    sort(ptr->workshops.begin(), ptr->workshops.end(), later);
+    vector<Candidate> candidates;
+    for (const Workshop workshop : ptr->workshops)
+    {
+        auto not_conflict = after(workshop.end_time);
+        if (!any_of(candidates.begin(), candidates.end(), not_conflict))
+        {
+            candidates.push_back(Candidate(workshop.start_time, 1));
+        }
+        const int size = candidates.size();
+        for (int index = 0; index < size; index++)
+        {
+            if (not_conflict(candidates[index]))
+            {
+                candidates[index].start_time = workshop.start_time;
+                candidates[index].count++;
+            }
+        }
+    }
+    return reduce<Candidate, int>(reducer, candidates, 0);
 };
 
 int main(int argc, char *argv[])
@@ -81,7 +132,6 @@ int main(int argc, char *argv[])
     {
         cin >> duration[i];
     }
-
     Available_Workshops *ptr;
     ptr = initialize(start_time, duration, n);
     cout << CalculateMaxWorkshops(ptr) << endl;
